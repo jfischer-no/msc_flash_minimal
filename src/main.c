6730bd9d41fead88b19fe3f61321d46e8716558e
Johann Fischer <johann.fischer@nordicsemi.no>
2023-06-08 11:55:08 +0200

    Add a minimal MSC sample using QSPI flash.
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>

---
+++ src/main.c
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2016 Intel Corporation.
+ * Copyright (c) 2019-2023 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/usb/class/usbd_msc.h>
+#include <zephyr/fs/fs.h>
+#include <stdio.h>
+
+#include <zephyr/storage/flash_map.h>
+#include <ff.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main);
+
+#define STORAGE_PARTITION		storage_partition
+#define STORAGE_PARTITION_ID		FIXED_PARTITION_ID(STORAGE_PARTITION)
+
+static struct fs_mount_t fs_mnt;
+
+static int setup_flash(struct fs_mount_t *mnt)
+{
+	int rc = 0;
+	unsigned int id;
+	const struct flash_area *pfa;
+
+	mnt->storage_dev = (void *)STORAGE_PARTITION_ID;
+	id = STORAGE_PARTITION_ID;
+
+	rc = flash_area_open(id, &pfa);
+	printk("Area %u at 0x%x on %s for %u bytes\n",
+	       id, (unsigned int)pfa->fa_off, pfa->fa_dev->name,
+	       (unsigned int)pfa->fa_size);
+
+	if (rc < 0) {
+		flash_area_close(pfa);
+	}
+
+	return rc;
+}
+
+static int mount_app_fs(struct fs_mount_t *mnt)
+{
+	static FATFS fat_fs;
+
+	mnt->type = FS_FATFS;
+	mnt->fs_data = &fat_fs;
+	mnt->mnt_point = "/NAND:";
+
+	return fs_mount(mnt);
+}
+
+static int setup_disk(void)
+{
+	struct fs_mount_t *mp = &fs_mnt;
+	struct fs_dir_t dir;
+	struct fs_statvfs sbuf;
+	int rc;
+
+	fs_dir_t_init(&dir);
+
+	rc = setup_flash(mp);
+	if (rc < 0) {
+		LOG_ERR("Failed to setup flash area");
+		return rc;
+	}
+
+	rc = mount_app_fs(mp);
+	if (rc < 0) {
+		LOG_ERR("Failed to mount filesystem");
+		return rc;
+	}
+
+	/* Allow log messages to flush to avoid interleaved output */
+	k_sleep(K_MSEC(50));
+
+	printk("Mount %s: %d\n", fs_mnt.mnt_point, rc);
+
+	rc = fs_statvfs(mp->mnt_point, &sbuf);
+	if (rc < 0) {
+		printk("FAIL: statvfs: %d\n", rc);
+		return rc;
+	}
+
+	printk("%s: bsize = %lu ; frsize = %lu ;"
+	       " blocks = %lu ; bfree = %lu\n",
+	       mp->mnt_point,
+	       sbuf.f_bsize, sbuf.f_frsize,
+	       sbuf.f_blocks, sbuf.f_bfree);
+
+	rc = fs_opendir(&dir, mp->mnt_point);
+	printk("%s opendir: %d\n", mp->mnt_point, rc);
+
+	if (rc < 0) {
+		LOG_ERR("Failed to open directory");
+	}
+
+	while (rc >= 0) {
+		struct fs_dirent ent = { 0 };
+
+		rc = fs_readdir(&dir, &ent);
+		if (rc < 0) {
+			LOG_ERR("Failed to read directory entries");
+			break;
+		}
+		if (ent.name[0] == 0) {
+			printk("End of files\n");
+			break;
+		}
+		printk("  %c %u %s\n",
+		       (ent.type == FS_DIR_ENTRY_FILE) ? 'F' : 'D',
+		       ent.size,
+		       ent.name);
+	}
+
+	(void)fs_closedir(&dir);
+
+	return rc;
+}
+
+int main(void)
+{
+	int ret;
+
+	ret = setup_disk();
+	if (ret < 0) {
+		LOG_ERR("Failed to setup flash disk");
+		return 0;
+	}
+
+	ret = usb_enable(NULL);
+	if (ret != 0) {
+		LOG_ERR("Failed to enable USB");
+		return 0;
+	}
+
+	LOG_INF("The device is put in USB mass storage mode.\n");
+
+	return ret;
+}
